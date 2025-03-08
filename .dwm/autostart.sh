cedde2b8c0b4273a1d1df78c8345f9fdb7cf15b5
Daniel Liu <daniel17liu@gmail.com>
2025-03-08 00:28:34 -0800

    everblush for statusbar as well

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,37 +1,37 @@
-#!/bin/bash
-black=#020203
-gray=#1c252c
-white=#e3e6eb
-red=#e05f65
-orange=#f1cf8a
-green=#78dba9
-cyan0=#74bee9
-cyan1=#69add4
-blue=#70a5eb
-purple=#c68aee
+#!/usr/bin/env bash
+
+black=#141b1e
+gray=#232a2d
+white=#dadada
+red=#e57474
+orange=#e5c76b
+green=#8ccf7e
+cyan=#67b0e8
+blue=#67b0e8
+purple=#c47fd5
 
 function media() {
-	status=`playerctl status`
+        status=`playerctl status`
   icon=""
   if [[ "$status" == "Playing" ]]; then
     icon=""
   elif [[ "$status" == "Paused" ]]; then
     icon=""
   fi
-	if [[ -z "$status" ]]; then
-		printf "^c$black^^b$blue^ ${icon} ^d^%s""^c$white^^b$gray^ No Players ^b$black^"
-	else
-		metadata=`playerctl metadata --format '{{artist}} • {{title}}' | sed 's/\(.\{25\}\).*/\1.../'`
-		printf "^c$black^^b$blue^ ${icon} ^d^%s""^c$white^^b$gray^ ${metadata} ^b$black^"
-	fi
+        if [[ -z "$status" ]]; then
+                printf "^c$black^^b$blue^ ${icon} ^d^%s""^c$white^^b$gray^ No Players ^b$black^"
+        else
+                metadata=`playerctl metadata --format '{{artist}} • {{title}}' | sed 's/\(.\{25\}\).*/\1.../'`
+                printf "^c$black^^b$blue^ ${icon} ^d^%s""^c$white^^b$gray^ ${metadata} ^b$black^"
+        fi
 }
 
 function wifi() {
   wifi=`nmcli -t -f active,ssid dev wifi | grep -E '^yes' | cut -d\' -f2 | cut -b 5-`
   if [[ -z "$wifi" ]]; then
-	  printf "^c$black^^b$purple^  ^d^%s""^c$white^^b$gray^ Disconnected ^b$black^"
+          printf "^c$black^^b$purple^  ^d^%s""^c$white^^b$gray^ Disconnected ^b$black^"
   else
-	  printf "^c$black^^b$purple^  ^d^%s""^c$white^^b$gray^ ${wifi} ^b$black^"
+          printf "^c$black^^b$purple^  ^d^%s""^c$white^^b$gray^ ${wifi} ^b$black^"
   fi
 }
 
@@ -48,7 +48,7 @@ function vol() {
   if [[ "$mute" == "true" ]]; then
     printf "^c$blue^ 婢Muted "
   else
-    printf "^c$blue^ 墳$(pamixer --get-volume)%% "
+   printf "^c$blue^ 墳$(pamixer --get-volume)%% "
   fi
 }
 
@@ -57,7 +57,7 @@ function mem() {
 }
 
 function dat() {
-  printf "^c$black^^b$cyan0^ 󰃰 ^d^%s""^c$black^^b$cyan1^ $(date '+%b %d, %I:%M %p') ^b$black^"
+  printf "^c$black^^b$cyan^ 󰃰 ^d^%s""^c$white^^b$gray^ $(date '+%b %d, %I:%M %p') ^b$black^"
 }
 
 function bat() {
@@ -68,11 +68,6 @@ function bat() {
   fi
   printf " ^c$green^${icon} $(cat /sys/class/power_supply/BAT0/capacity)%% "
 }
-
-~/.fehbg &
-dunst &
-xfsettingsd &
-picom &
 
 while true; do
   xsetroot -name " $(bat) $(vol) $(bri) $(media) $(wifi) $(dat)"
